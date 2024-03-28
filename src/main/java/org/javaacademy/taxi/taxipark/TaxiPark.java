534d87e0dc9573909a21018b8920f19dc4b8dbcc
dash01 <damir@shakirov.tv>
2024-03-28 11:18:11 +0300

    перенес переменные и методы в родительский класс TaxiPark

--- src/main/java/org/javaacademy/taxi/taxipark/TaxiPark.java
+++ src/main/java/org/javaacademy/taxi/taxipark/TaxiPark.java
@@ -1,18 +1,55 @@
 package org.javaacademy.taxi.taxipark;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.javaacademy.taxi.client.Client;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
 import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.Queue;
 
+@Slf4j
 @Getter
-public abstract class TaxiPark {
+public abstract class TaxiPark implements ApplicationListener<ContextClosedEvent> {
     @Value("${taxipark.name}")
     private String name;
     @Setter
     private BigDecimal income = BigDecimal.ZERO;
+    protected Queue<Taxi> taxiQueue = new LinkedList<>();
 
-    public abstract void addIncome(BigDecimal incomeByTaxi);
-    public abstract void acceptOrder(Client client, TimeOfDay timeOfDay);
+    public void acceptOrder(Client client, TimeOfDay timeOfDay) {
+        log.info("Таксопарк принял заказ на адрес: {}", client.getAddress());
+        Taxi taxi = taxiQueue.remove();
+        transferOrderToTaxi(taxi, client, timeOfDay);
+        taxiQueue.add(taxi);
+    }
+
+    private void transferOrderToTaxi(Taxi taxi, Client client, TimeOfDay timeOfDay) {
+        try {
+            taxi.acceptOrder(client, timeOfDay);
+        } catch (RuntimeException e) {
+            log.error("Заказ отменен. Неизвестный адрес");
+        }
+    }
+
+    public void addIncome(BigDecimal incomeByTaxi) {
+        this.setIncome(this.getIncome().add(incomeByTaxi));
+    }
+
+    public void setTaxiQueue(Taxi taxi) {
+        this.taxiQueue.add(taxi);
+    }
+
+    private void report() {
+        log.info("Итог работы таксопарка. Имя таксопарка: {}, заработано: {} руб.", this.getName(), this.getIncome());
+        taxiQueue.forEach(taxi -> log.info("Водитель машины №{} заработал: {} руб.", taxi.getNumber(), taxi.getIncome()));
+    }
+
+    public void onApplicationEvent(@NonNull ContextClosedEvent event) {
+        report();
+    }
 }
--- src/main/java/org/javaacademy/taxi/taxipark/TaxiParkBluz.java
+++ src/main/java/org/javaacademy/taxi/taxipark/TaxiParkBluz.java
@@ -1,57 +1,15 @@
 package org.javaacademy.taxi.taxipark;
 
-import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
-import org.javaacademy.taxi.client.Client;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.stereotype.Component;
-import java.math.BigDecimal;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 
-@Slf4j
 @Profile("bluz")
 @Component
-public class TaxiParkBluz extends TaxiPark implements ApplicationListener<ContextClosedEvent> {
-    private final Queue<Taxi> taxiQueue = new LinkedList<>();
+public class TaxiParkBluz extends TaxiPark {
 
     public TaxiParkBluz(@Lazy Taxi taxi1, @Lazy Taxi taxi2) {
-        super();
-        this.taxiQueue.addAll(List.of(taxi1, taxi2));
-    }
-
-    @Override
-    public void addIncome(BigDecimal incomeByTaxi) {
-        this.setIncome(this.getIncome().add(incomeByTaxi));
-    }
-
-    @Override
-    public void acceptOrder(Client client, TimeOfDay timeOfDay) {
-        log.info("Таксопарк принял заказ на адрес: {}", client.getAddress());
-        Taxi taxi = taxiQueue.remove();
-        transferOrderToTaxi(taxi, client, timeOfDay);
-        taxiQueue.add(taxi);
-    }
-
-    private void report() {
-        log.info("Итог работы таксопарка. Имя таксопарка: {}, заработано: {} руб.", this.getName(), this.getIncome());
-        taxiQueue.forEach(taxi -> log.info("Водитель машины №{} заработал: {} руб.", taxi.getNumber(), taxi.getIncome()));
-    }
-
-    private void transferOrderToTaxi(Taxi taxi, Client client, TimeOfDay timeOfDay) {
-        try {
-            taxi.acceptOrder(client, timeOfDay);
-        } catch (RuntimeException e) {
-            log.error("Заказ отменен. Неизвестный адрес");
-        }
-    }
-
-    @Override
-    public void onApplicationEvent(@NonNull ContextClosedEvent event) {
-        report();
+        setTaxiQueue(taxi1);
+        setTaxiQueue(taxi2);
     }
 }
